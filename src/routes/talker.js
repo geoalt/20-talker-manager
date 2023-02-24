cf074173bebc036a33aa86d4b15f6f0554632c13
Diogo Martins <me.martinsfe@gmail.com>
2023-02-24 02:43:15 -0300

    style: barrel functions

--- src/routes/talker.js
+++ src/routes/talker.js
@@ -1,25 +1,23 @@
 const express = require('express');
-const utils = require('../utils/fs');
+const { fs } = require('../utils');
 
 const talker = express.Router();
 
 const filePath = '../talker.json';
 
 talker.get('/', async (_req, res) => {
-  const talkersData = await utils.readFile(filePath);
+  const talkersData = await fs.readFile(filePath);
   return res.status(200).json(talkersData);
 });
 
 talker.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const talkersData = await utils.readFile(filePath);
+  const talkersData = await fs.readFile(filePath);
 
   const findTalkerById = talkersData.find((it) => it.id === Number(id));
 
   if (!findTalkerById) {
-    return res
-      .status(404)
-      .json({ message: 'Pessoa palestrante não encontrada' });
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
 
   res.status(200).json(findTalkerById);
---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+const fs = require('./fs');
+const tokenGenerator = require('./tokenGenerator');
+
+module.exports = {
+  fs,
+  tokenGenerator,
+};
