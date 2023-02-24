4f41fe7bbbdeb6fddd9bd0f028e13f854a9d6c0e
Diogo Martins <me.martinsfe@gmail.com>
2023-02-23 23:59:57 -0300

    feat: add option to find a talker by id

--- src/routes/talker.js
+++ src/routes/talker.js
@@ -7,7 +7,22 @@ const filePath = '../talker.json';
 
 talker.get('/', async (req, res) => {
   const talkersData = await utils.readFile(filePath);
-  res.status(200).json(talkersData);
+  return res.status(200).json(talkersData);
+});
+
+talker.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkersData = await utils.readFile(filePath);
+
+  const findTalkerById = talkersData.find((it) => it.id === Number(id));
+
+  if (!findTalkerById) {
+    return res
+      .status(404)
+      .json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(200).json(findTalkerById);
 });
 
 module.exports = talker;
